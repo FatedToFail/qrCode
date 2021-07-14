085592a0bef0fbf0bb54a50b4a9be548a0695be4
aparniczky <aronparniczky@Arons-MacBook-Pro.local>
2021-07-14 13:59:04 +0200

    feat: expand testcases

--- qrCodeGenerator/qrCodeGenerator.test.js
+++ qrCodeGenerator/qrCodeGenerator.test.js
@@ -1,29 +1,47 @@
 const testData = require('./testdata.json');
 const qrCodeGenerator = require('./qrCodeGenerator');
 
-const { item1, item2, config, discounts, itemPrices, sale1, sale2 } = testData;
+const { item1, item2, config, discounts, itemPrices, sale1, sale2, sale3 } = testData;
 
-test('qr for V5 with cash and no discounts', () => {
-  expect(qrCodeGenerator(config, sale2, [item1, item2], itemPrices, discounts, 5))
-    .toBe('http://www.fiscat.com/AEE|SLD|||4||||||||2||3|500|1|||3|1000|2||N');
+describe('qr code for V5', () => {
+  test('WITH cash and WITH discounts - 1 item', () => {
+    expect(qrCodeGenerator(config, sale1, [item1, item2], itemPrices, discounts, 5))
+      .toBe('http://www.fiscat.com/AEE|SLD|||4||||||||1||3|1000|2|M20|P');
+  });
+  test('WITH card and NO discounts - 2 items', () => {
+    expect(qrCodeGenerator(config, sale2, [item1, item2], itemPrices, discounts, 5))
+      .toBe('http://www.fiscat.com/AEE|SLD|||4||||||||2||3|500|1|||3|1000|2||N');
+  });
+  test('WITH card and WITH discounts - 3 items', () => {
+    expect(qrCodeGenerator(config, sale3, [item1, item2], itemPrices, discounts, 5))
+      .toBe('http://www.fiscat.com/AEE|SLD|||4||||||||3||3|500|1|||3|1000|2|||3|500|3|M20|N');
+  });
 });
-test('qr for V5 with cash and with discounts', () => {
-  expect(qrCodeGenerator(config, sale1, [item1, item2], itemPrices, discounts, 5))
-    .toBe('http://www.fiscat.com/AEE|SLD|||4||||||||2||3|500|1|||3|1000|2|M20|P');
+describe('qr code for V4', () => {
+  test('WITH cash and WITH discounts - 1 item', () => {
+    expect(qrCodeGenerator(config, sale1, [item1, item2], itemPrices, discounts, 4))
+      .toBe('http://www.fiscat.com/AEE|SLD|||4||||||||1||3|1000|2|P');
+  });
+  test('WITH card and NO discounts - 2 items', () => {
+    expect(qrCodeGenerator(config, sale2, [item1, item2], itemPrices, discounts, 4))
+      .toBe('http://www.fiscat.com/AEE|SLD|||4||||||||2||3|500|1||3|1000|2|N');
+  });
+  test('WITH card and WITH discounts - 3 items', () => {
+    expect(qrCodeGenerator(config, sale3, [item1, item2], itemPrices, discounts, 4))
+      .toBe('http://www.fiscat.com/AEE|SLD|||4||||||||3||3|500|1||3|1000|2||3|500|3|N');
+  });
 });
-test('qr for V4 with cash and no discounts', () => {
-  expect(qrCodeGenerator(config, sale2, [item1, item2], itemPrices, discounts, 4))
-    .toBe('http://www.fiscat.com/AEE|SLD|||4||||||||2||3|500|1||3|1000|2|N');
-});
-test('qr for V4 with cash and with discounts', () => {
-  expect(qrCodeGenerator(config, sale1, [item1, item2], itemPrices, discounts, 4))
-    .toBe('http://www.fiscat.com/AEE|SLD|||4||||||||2||3|500|1||3|1000|2|P');
-});
-test('qr for V3 with cash and no discounts', () => {
-  expect(qrCodeGenerator(config, sale2, [item1, item2], itemPrices, discounts, 3))
-    .toBe('http://www.fiscat.com/AEE|SLD||4||||||||2||3|500|1||3|1000|2|N');
-});
-test('qr for V3 with cash and with discounts', () => {
-  expect(qrCodeGenerator(config, sale1, [item1, item2], itemPrices, discounts, 3))
-    .toBe('http://www.fiscat.com/AEE|SLD||4||||||||2||3|500|1||3|1000|2|P');
+describe('qr code for V3', () => {
+  test('WITH cash and WITH discounts - 1 item', () => {
+    expect(qrCodeGenerator(config, sale1, [item1, item2], itemPrices, discounts, 3))
+      .toBe('http://www.fiscat.com/AEE|SLD||4||||||||1||3|1000|2|P');
+  });
+  test('WITH card and NO discounts - 2 items', () => {
+    expect(qrCodeGenerator(config, sale2, [item1, item2], itemPrices, discounts, 3))
+      .toBe('http://www.fiscat.com/AEE|SLD||4||||||||2||3|500|1||3|1000|2|N');
+  });
+  test('WITH card and WITH discounts - 3 items', () => {
+    expect(qrCodeGenerator(config, sale3, [item1, item2], itemPrices, discounts, 3))
+      .toBe('http://www.fiscat.com/AEE|SLD||4||||||||3||3|500|1||3|1000|2||3|500|3|N');
+  });
 });
